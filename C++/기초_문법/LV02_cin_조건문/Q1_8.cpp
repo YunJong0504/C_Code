5cb122c07b3e7b894b89e86975f7f19cf7471ad1
YunJong <lye730856@gmail.com>
2026-09-21 21:14:31 +0900

    Create Q1_8.cpp

---
+++ C++/기초_문법/LV02_cin_조건문/Q1_8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int a, b;
+	std::cin >> a >> b;
+
+	if (a > b)
+	{
+		std::cout << a << a << a << a;
+	}
+	else
+	{
+		std::cout << b << b << b << b;
+	}
+
+	return 0;
+}
