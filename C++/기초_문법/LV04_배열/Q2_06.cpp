e4cf94ef4ae30792e5b7ca85aab0a94e8455b77c
YunJong <lye730856@gmail.com>
2026-09-21 21:27:51 +0900

    Create Q2_06.cpp

---
+++ C++/기초_문법/LV04_배열/Q2_06.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	int arr[5] = { 3,9,12,15,55 };
+
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	
+	if ((a + b + c) > 10)
+	{
+		std::cout << arr[4];
+	}
+	else
+	{
+		std::cout << arr[0];
+	}
+
+	return 0;
+}
