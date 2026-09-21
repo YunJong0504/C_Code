1287e70b6c0d04d58a7f3432ae991684fc60d056
YunJong <lye730856@gmail.com>
2026-09-21 22:12:06 +0900

    Update Q1_4.cpp

--- C++/고급_알고리즘/LV09_완전탐색_백트래킹_Flood_Fill/Q1_4.cpp
+++ C++/고급_알고리즘/LV09_완전탐색_백트래킹_Flood_Fill/Q1_4.cpp
@@ -2,6 +2,11 @@
 #include <queue>
 using namespace std;
 
+struct Node
+{
+    int y, x;
+};
+
 int main()
 {
     int map[4][5];
@@ -9,7 +14,7 @@ int main()
     int dy[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
     int dx[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
-    queue<pair<int, int>> q;
+    queue<Node> q;
 
     for (int y = 0; y < 4; y++)
     {
@@ -32,18 +37,15 @@ int main()
 
         for (int i = 0; i < size; i++)
         {
-            int y = q.front().first;
-            int x = q.front().second;
-
+            Node n = q.front();
             q.pop();
 
-            for (int dir = 0; dir < 8; dir++)
+            for (int j = 0; j < 8; j++)
             {
-                int ny = y + dy[dir];
-                int nx = x + dx[dir];
+                int ny = n.y + dy[j];
+                int nx = n.x + dx[j];
 
-                if (ny < 0 || ny >= 4 ||
-                    nx < 0 || nx >= 5)
+                if (ny < 0 || ny >= 4 || nx < 0 || nx >= 5)
                 {
                     continue;
                 }
