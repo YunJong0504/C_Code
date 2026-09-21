a86bc0b50ab6bbc149fecda76cbc3303d51ed714
YunJong <lye730856@gmail.com>
2026-09-21 21:15:56 +0900

    Create Q2_3.cpp

---
+++ C++/기초_문법/LV02_cin_조건문/Q2_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+
+	std::cout << "첫번째 숫자는 " << a << "입니다\n";
+	std::cout << "두번째 숫자는 " << b << "입니다\n";
+	std::cout << "세번째 숫자는 " << c << "입니다\n";
+
+	return 0;
+}
