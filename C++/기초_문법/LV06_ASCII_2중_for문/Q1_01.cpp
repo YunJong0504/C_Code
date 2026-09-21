8bcb2ddb0cf92449d0b9ba9ffe902018f3db5abc
YunJong <lye730856@gmail.com>
2026-09-21 22:19:46 +0900

    Add basic ASCII input/output program in C++

---
+++ C++/기초_문법/LV06_ASCII_2중_for문/Q1_01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+char ch1, ch2;
+
+void ABC()
+{
+	std::cin >> ch1 >> ch2;
+}
+
+void OUTPUT()
+{
+	std::cout << ch1 << " " << ch2;
+}
+
+int main()
+{
+	ABC();
+	OUTPUT();
+
+	return 0;
+}
