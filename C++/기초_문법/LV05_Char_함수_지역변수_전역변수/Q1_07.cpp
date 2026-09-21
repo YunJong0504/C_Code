4933344e0436b6bdd3c133b3f8161a51c9d9df43
YunJong <lye730856@gmail.com>
2026-09-21 21:30:23 +0900

    Create Q1_07.cpp

---
+++ C++/기초_문법/LV05_Char_함수_지역변수_전역변수/Q1_07.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	int arr[6] = {};
+	std::cin >> arr[0] >> arr[1] >> arr[2];
+
+	int num;
+	std::cin >> num;
+
+	for (int i = 3; i < 6; i++)
+	{
+		arr[i] = num;
+		num++;
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}
