a12b0b85362e9c31afb36685cf7c4dcb71e704a6
YunJong <lye730856@gmail.com>
2026-09-21 21:13:40 +0900

    Create Q1_4.cpp

---
+++ C++/기초_문법/LV02_cin_조건문/Q1_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+
+	std::cout << a << a << a << "\n";
+	std::cout << b << b << b << "\n";
+	std::cout << c << c << c << "\n";
+
+	return 0;
+}
