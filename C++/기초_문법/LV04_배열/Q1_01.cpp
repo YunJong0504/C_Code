dc962ea663d116a980f733cc0d3f601066ec602e
YunJong <lye730856@gmail.com>
2026-09-21 21:25:00 +0900

    Add array input and output in Q1_01.cpp
    Implement basic array input and output functionality.

---
+++ C++/기초_문법/LV04_배열/Q1_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int arr[5] = {};
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cin >> arr[i];
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}
