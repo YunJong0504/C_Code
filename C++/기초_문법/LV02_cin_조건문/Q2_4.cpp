7b11e823a3cca12be084fd7bcbd8f4e0120c9928
YunJong <lye730856@gmail.com>
2026-09-21 21:16:08 +0900

    Create Q2_4.cpp

---
+++ C++/기초_문법/LV02_cin_조건문/Q2_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c, d;
+	std::cin >> a >> b >> c >> d;
+
+	std::cout << "a + b = " << a + b << std::endl;
+	std::cout << "c + d = " << c + d << std::endl;
+	std::cout << "ALL SUM = " << a + b + c + d;
+
+	return 0;
+}
