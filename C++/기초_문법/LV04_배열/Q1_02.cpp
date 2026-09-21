6f61dc200686121ba81e4b903eefeb97a25e7220
YunJong <lye730856@gmail.com>
2026-09-21 21:25:12 +0900

    Create Q1_02.cpp

---
+++ C++/기초_문법/LV04_배열/Q1_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int arr[5] = { 4,1,3,6,9 };
+
+	int index;
+	std::cin >> index;
+	
+	std::cout << index << "번index의값은" << arr[index] << "입니다.";
+
+	return 0;
+}
