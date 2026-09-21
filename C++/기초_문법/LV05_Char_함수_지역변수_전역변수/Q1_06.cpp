8c0bdb848bf90302763ec3bca5409796acdc608c
YunJong <lye730856@gmail.com>
2026-09-21 21:30:15 +0900

    Create Q1_06.cpp

---
+++ C++/기초_문법/LV05_Char_함수_지역변수_전역변수/Q1_06.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void KFC()
+{
+	std::cout << "##########" << std::endl;
+}
+
+void MC()
+{
+	std::cout << "@@@@@@@@@@" << std::endl;
+}
+
+int main()
+{
+	KFC();
+	MC();
+
+	return 0;
+}
