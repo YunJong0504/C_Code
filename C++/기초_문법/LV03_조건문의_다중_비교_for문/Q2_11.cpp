980d9b5ca9748acaac3ebde8c6e9fef6f29f2005
YunJong <lye730856@gmail.com>
2026-09-21 21:23:06 +0900

    Create Q2_11.cpp

---
+++ C++/기초_문법/LV03_조건문의_다중_비교_for문/Q2_11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "시작" << std::endl;
+
+	for (int i = 1; i <= 5; i++)
+	{
+		std::cout << i;
+	}
+	std::cout << std::endl;
+
+	std::cout << "종료";
+
+	return 0;
+}
