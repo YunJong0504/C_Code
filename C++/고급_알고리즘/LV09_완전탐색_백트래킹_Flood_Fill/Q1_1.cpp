0708b2ac311c024303dcb9699afa1d571e6bbc92
YunJong <lye730856@gmail.com>
2026-09-21 21:50:57 +0900

    Update Q1_1.cpp

--- C++/고급_알고리즘/LV09_완전탐색_백트래킹_Flood_Fill/Q1_1.cpp
+++ C++/고급_알고리즘/LV09_완전탐색_백트래킹_Flood_Fill/Q1_1.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+struct Node
+{
+    int y;
+    int x;
+};
+
 int map[4][4] =
 {
     {0, 0, 0, 0},
@@ -11,60 +17,52 @@ int map[4][4] =
     {1, 0, 1, 0}
 };
 
-int visited[4][4];
+int visited[4][4] = {};
 
 int dx[4] = { 0, 0, -1, 1 };
 int dy[4] = { -1, 1, 0, 0 };
 
 int main()
 {
-    int startX, startY;
-    int endX, endY;
+    int startY, startX;
+    int endY, endX;
 
     cin >> startX >> startY;
     cin >> endX >> endY;
 
-    queue<pair<int, int>> q;
+    queue<Node> q;
 
-    q.push({ startX, startY });
+    q.push({ startY, startX });
     visited[startY][startX] = 1;
 
     while (!q.empty())
     {
-        int x = q.front().first;
-        int y = q.front().second;
+        Node now = q.front();
         q.pop();
 
-        if (x == endX && y == endY)
+        if (now.y == endY && now.x == endX)
         {
-            cout << visited[y][x] << endl;
+            cout << visited[now.y][now.x] - 1 << endl;
             return 0;
         }
 
         for (int i = 0; i < 4; i++)
         {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int ny = now.y + dy[i];
+            int nx = now.x + dx[i];
 
-            if (nx < 0 || nx >= 4 ||
-                ny < 0 || ny >= 4)
-            {
+            if (ny < 0 || ny >= 4 || nx < 0 || nx >= 4)
                 continue;
-            }
 
             if (map[ny][nx] == 1)
-            {
                 continue;
-            }
 
             if (visited[ny][nx] != 0)
-            {
                 continue;
-            }
 
-            visited[ny][nx] = visited[y][x] + 1;
+            visited[ny][nx] = visited[now.y][now.x] + 1;
 
-            q.push({ nx, ny });
+            q.push({ ny, nx });
         }
     }
 
