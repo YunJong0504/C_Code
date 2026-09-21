8dd46d027159a2404df2f5814ee45b3287ee34f3
YunJong <lye730856@gmail.com>
2026-09-21 22:20:02 +0900

    Create Q1_02.cpp

---
+++ C++/기초_문법/LV06_ASCII_2중_for문/Q1_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	char ch1, ch2, ch3;
+	std::cin >> ch1 >> ch2 >> ch3;
+
+	std::cout << (int)ch1 << std::endl;
+	std::cout << (int)ch2 << std::endl;
+	std::cout << (int)ch3;
+
+	return 0;
+}
