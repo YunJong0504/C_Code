fee51900de83c3553b3eda6a26b220337a8a2f71
YunJong <lye730856@gmail.com>
2026-09-21 21:16:21 +0900

    Create Q2_5.cpp

---
+++ C++/기초_문법/LV02_cin_조건문/Q2_5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+
+	if (a == b == c)
+	{
+		std::cout << "만세";
+	}
+	else
+	{
+		std::cout << "";
+	}
+
+	return 0;
+}
