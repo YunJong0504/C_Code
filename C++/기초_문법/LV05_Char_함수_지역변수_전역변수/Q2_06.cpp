8782702500d4840242f4e3c037c47f63fc85b325
YunJong <lye730856@gmail.com>
2026-09-21 21:32:22 +0900

    Add Q2_06.cpp to print array elements between indices

---
+++ C++/기초_문법/LV05_Char_함수_지역변수_전역변수/Q2_06.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int arr[7] = { 3,5,1,2,4,6,7 };
+	int a, b;
+	std::cin >> a >> b;
+
+	for (int i = a; i <= b; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}
