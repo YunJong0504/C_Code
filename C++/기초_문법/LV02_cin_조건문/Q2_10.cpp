129dc4cb0e2dc1cbcd56c6082ebae108bd7fc66e
YunJong <lye730856@gmail.com>
2026-09-21 21:17:22 +0900

    Create Q2_10.cpp

---
+++ C++/기초_문법/LV02_cin_조건문/Q2_10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c, d, e, f, g;
+	std::cin >> a >> b >> c >> d >> e >> f >> g;
+	std::cout << d << " " << e << " " << f << " " << g;
+
+	return 0;
+}
