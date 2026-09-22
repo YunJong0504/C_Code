c2b79c3e8ea8e858a60fd4d8ff8a4f737c0b9608
YunJong <lye730856@gmail.com>
2026-09-22 10:20:53 +0900

    Create Q1_05.cpp

---
+++ C++/기초_문법/LV10_포인터타입_변수/Q1_05.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int GOP()
+{
+	int a, b;
+	cin >> a >> b;
+	
+	return a * b;
+}
+
+int SUM()
+{
+	int a, b;
+	cin >> a >> b;
+
+	return a + b;
+}
+
+
+int main()
+{
+	int a = GOP();
+	int b = SUM();
+
+	if (a > b)
+		cout << "GOP>SUM";
+	else if (b > a)
+		cout << "GOP<SUM";
+	else
+		cout << "GOP==SUM";
+
+	return 0;
+}
