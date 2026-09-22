da6a57cf061069c088dd26a0fda568ece8096763
YunJong <lye730856@gmail.com>
2026-09-22 11:22:47 +0900

    Create Q1_06.cpp

---
+++ C++/기초_문법/LV13_포인터_변수의_원리와_문자열/Q1_06.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[6] = {};
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int flag = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		if ((arr[i] - arr[i + 1]) >= 3 || (arr[i] - arr[i + 1]) <= -3)
+		{
+			flag++;
+			break;
+		}
+	}
+
+	if (flag == 1)
+	{
+		cout << "재배치필요";
+	}
+	else if (flag == 0)
+	{
+		cout << "완벽한배치";
+	}
+
+	return 0;
+}
