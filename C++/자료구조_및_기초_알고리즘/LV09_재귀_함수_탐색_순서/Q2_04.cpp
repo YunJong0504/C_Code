c49ec213b4a722f312b68bab6ba65369d299ef70
YunJong <lye730856@gmail.com>
2026-09-22 17:16:51 +0900

    Create Q2_04.cpp

---
+++ C++/자료구조_및_기초_알고리즘/LV09_재귀_함수_탐색_순서/Q2_04.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int arr[3][2][3] = {};
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+	for (int z = 0; z < 3; z++)
+	{
+		for (int y = 0; y < 2; y++)
+		{
+			for (int x = 0; x < 3; x++)
+			{
+				if (y == 0)
+				{
+					arr[z][y][x] = a;
+				}
+				else if (y == 1)
+				{
+					arr[z][y][x] = b;
+				}
+			}
+		}
+	}
+	for (int z = 0; z < 3; z++)
+	{
+		for (int y = 0; y < 2; y++)
+		{
+			for (int x = 0; x < 3; x++)
+			{
+				cout << arr[z][y][x];
+			}
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
