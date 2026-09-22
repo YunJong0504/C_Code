fcf8d9d06660d6c8b095c2160b32deb256d1e889
YunJong <lye730856@gmail.com>
2026-09-22 11:30:23 +0900

    Create Q1_06.cpp

---
+++ C++/자료구조_및_기초_알고리즘/LV02_기본기_복습(마스킹_다중카운팅_인덱스_함수)/Q1_06.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int bit[2][5] = { {0,0,1,0,0},{0,0,1,1,1} };
+	int arr[2][5] = { {3,5,4,1,1},{3,5,2,5,6} };
+	int num;
+	cin >> num;
+
+	int flag = 0;
+	for (int y = 0; y < 2; y++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			if (arr[y][x] == num)
+			{
+				if (bit[y][x] == 1)
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+	}
+
+	if (flag == 1)
+		cout << num << " 존재";
+	else
+		cout << num << " 없음";
+
+	return 0;
+}
