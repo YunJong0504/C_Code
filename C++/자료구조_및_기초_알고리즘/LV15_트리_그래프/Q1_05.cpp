9ba5fcff535b59ff4e6c98994231bb1fdebb31a0
YunJong <lye730856@gmail.com>
2026-09-23 09:50:13 +0900

    Create Q1_05.cpp

---
+++ C++/자료구조_및_기초_알고리즘/LV15_트리_그래프/Q1_05.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int arr[4][4] = {};
+int direct[4][2] =
+{
+	-1,0,
+	0,-1,
+	0,1,
+	1,0
+};
+
+void Input()
+{
+	for (int y = 0; y < 4; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			cin >> arr[y][x];
+		}
+	}
+}
+
+int Find(int y, int x)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		int newY = y + direct[i][0];
+		int newX = x + direct[i][1];
+		if (newY >= 0 && newY <= 3 && newX >= 0 && newX <= 3)
+		{
+			if (arr[newY][newX] == 1)
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	Input();
+	int flag = 0;
+
+	for (int y = 0; y < 4; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			if (arr[y][x] == 1)
+			{
+				flag = Find(y, x);
+				if (flag == 1)
+				{
+					cout << "위험한 상태";
+					return 0;
+				}
+			}
+		}
+	}
+	std::cout << "안전한 상태";
+	
+	return 0;
+}
